1f97749afc386d6bafc6a2ff37eb97eac7eba338
Bradley Sacks <bradsacks99@gmail.com>
2020-09-20 22:30:49 -0700

    Added cowsay format

--- talk.sh
+++ talk.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 
 RUNONCE="false"
-voice=Tessa
+VOICE=Tessa
+FORMAT=""
 
-voices=('Alex' 'Alice' 'Alva' 'Amelie' 'Anna' 'Carmit' 'Damayanti' 'Daniel' 'Diego' 'Ellen' 'Fiona' 'Fred' 'Ioana' 'Joana' 'Jorge' 'Juan' 'Kanya' 'Karen' 'Kyoko' 'Laura' 'Lekha' 'Luca' 'Luciana' 'Maged' 'Mariska' 'Mei-Jia' 'Melina' 'Milena' 'Moira' 'Monica' 'Nora' 'Paulina' 'Samantha' 'Sara' 'Satu' 'Sin-ji' 'Tessa' 'Thomas' 'Ting-Ting' 'Veena' 'Victoria' 'Xander' 'Yelda' 'Yuna' 'Yuri' 'Zosia' 'Zuzana')
+VOICES=('Alex' 'Alice' 'Alva' 'Amelie' 'Anna' 'Carmit' 'Damayanti' 'Daniel' 'Diego' 'Ellen' 'Fiona' 'Fred' 'Ioana' 'Joana' 'Jorge' 'Juan' 'Kanya' 'Karen' 'Kyoko' 'Laura' 'Lekha' 'Luca' 'Luciana' 'Maged' 'Mariska' 'Mei-Jia' 'Melina' 'Milena' 'Moira' 'Monica' 'Nora' 'Paulina' 'Samantha' 'Sara' 'Satu' 'Sin-ji' 'Tessa' 'Thomas' 'Ting-Ting' 'Veena' 'Victoria' 'Xander' 'Yelda' 'Yuna' 'Yuri' 'Zosia' 'Zuzana')
+
+FORMATS=('beavis.zen' 'blowfish' 'bud-frogs' 'bunny' 'cheese' 'cower' 'daemon' 'default' 'dragon' 'dragon-and-cow' 'elephant' 'elephant-in-snake' 'eyes' 'flaming-sheep' 'ghostbusters' 'head-in' 'hellokitty' 'kiss' 'kitty' 'koala' 'kosh' 'luke-koala' 'meow' 'milk' 'moofasa' 'moose' 'mutilated' 'ren' 'satanic' 'sheep' 'skeleton' 'small' 'stegosaurus' 'stimpy' 'supermilker' 'surgery' 'telebears' 'three-eyes' 'turkey' 'turtle' 'tux' 'udder' 'vader' 'vader-koala' 'www')
 
 print_voices() {
-    for v in "${voices[@]}"; do
+    for v in "${VOICES[@]}"; do
         echo $v;
     done
+}
+
+print_cowsay_formats() {
+    for f in "${FORMATS[@]}"; do
+        echo $f
+    done
+}
+
+change_cowsay_format() {
+    while :
+    do
+        echo "Enter a format:";
+        read input
+        if [[ " ${FORMATS[@]} " =~ " ${input} " ]]; then
+            FORMAT=$input
+            echo "Format changed to $FORMAT"
+            break
+        fi
+    done
+    return
 }
 
 change_voice() {
@@ -16,8 +39,9 @@ change_voice() {
     do
         echo "Enter a valid voice:";
         read input
-        if [[ -n "${voices[$input]}" ]]; then
-            voice=$input
+        if [[ " ${VOICES[@]} " =~ " ${input} " ]]; then
+            VOICE=$input
+            echo "Voice changed to $VOICE"
             break
         fi
     done
@@ -29,9 +53,12 @@ usage() {
 -h to display this message
 -q to start in quiet mode
 -v [voice], set start voice
+-f [format], set cowsay format
 Type .v to change voice"
 Type .q to exit"
-Type .l to list voices"
+Type .lv to list voices"
+Type .f to chagne cowsay format
+Type .lf to list cowsay formats
 EOF
 }
 
@@ -40,37 +67,63 @@ case $1 in
         RUNONCE="true"        
         ;;
     "-v")
-        if [[ -n "${voices[$2]}" ]]; then
-            voice=$2
+        if [[ -n "${VOICES[$2]}" ]]; then
+            VOICE=$2
         fi
         ;;
     "-h")
         usage
         exit 0
         ;;
+    "-f")
+        FORMAT=$2
+        ;;
 esac
 
 while :
 do
     if [[ $RUNONCE == "false" ]]; then
-        say -v $voice "What do you want the computer to say?"
-        say -v $voice "Type .v to change voice"
-        say -v $voice "Type .q to exit"
-        say -v $voice "Type .l to list voices"
+        say -v $VOICE "What do you want the computer to say?"
+        say -v $VOICE "Type .v to change voice"
+        say -v $VOICE "Type .q to exit"
+        say -v $VOICE "Type .lv to list voices"
+        say -v $VOICE "Type .lf to list cowsay formats"
     fi
+
     echo "Type here:";
+    
     RUNONCE="true"
+    
     read input
-    if [[ $input == ".q" ]]; then
-        exit 0
+    
+    case $input in
+        ".q")
+            exit 0        
+            ;;
+        ".v")
+            change_voice
+            continue
+            ;;
+        ".lv")
+            print_voices
+            continue
+            ;;
+        ".f")
+            change_cowsay_format 
+            continue
+            ;;
+        ".lf")
+            print_cowsay_formats 
+            continue
+            ;;
+    esac
+
+
+    FORMAT_STRING="" 
+    if [ -n "$FORMAT" ]; then
+        FORMAT_STRING="-f $FORMAT"
     fi
-    if [[ $input == ".v" ]]; then
-        change_voice
-    fi
-    if [[ $input == ".l" ]]; then
-        print_voices
-        continue
-    fi
-    cowsay $input
-    say -v $voice $input
+
+    cowsay $FORMAT_STRING $input
+    say -v $VOICE $input
 done
